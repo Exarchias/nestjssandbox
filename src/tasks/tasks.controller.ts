14884ff4cebbed05c36d59d177a3a3cbf61883f1
Robert Kristian Alm <robert.alm0018@stud.hkr.se>
2022-12-28 04:35:12 +0100

    L15 GetAllTasks
    This feature is implemented on Tasks Service and tasks controller. On tasks controller the handler @Get() was introduced.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common/decorators';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks;
+  }
 }
